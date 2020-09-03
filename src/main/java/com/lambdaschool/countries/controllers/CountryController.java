f4a44eb57261e6039e2640e280d7559c414f6bb0
Alex Williams <alexjwilliams1@gmail.com>
2020-09-03 16:05:34 -0700

    finished mvp

--- src/main/java/com/lambdaschool/countries/controllers/CountryController.java
+++ src/main/java/com/lambdaschool/countries/controllers/CountryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -25,9 +26,10 @@ public class CountryController
         {
             if(tester.test(c))
             {
-                tempList.add(c)
+                tempList.add(c);
             }
         }
+        return tempList;
     }
 
 
@@ -49,10 +51,62 @@ public class CountryController
     }
 
     // http://localhost:2019/names/start/u
-    
+    @GetMapping(value = "/names/start/{letter}", produces = {"application/json"})
+    public ResponseEntity<?> listAllByFirstLetter(@PathVariable char letter)
+    {
+        List<Country> myList = new ArrayList<>();
+        countryrepos.findAll().iterator().forEachRemaining(myList::add);
+
+        List<Country> rtnList = screenCountry(myList, e -> e.getName().charAt(0) == Character.toUpperCase(letter));
+
+        return new ResponseEntity<>(rtnList, HttpStatus.OK);
+    }
 
     // http://localhost:2019/population/total
-    // http://localhost:2019/population/min
-    // http://localhost:2019/population/max
 
+    @GetMapping(value = "/population/total", produces = {"application/json"})
+    public ResponseEntity<?> displayTotalPop()
+    {
+        List<Country> myList = new ArrayList<>();
+
+        countryrepos.findAll().iterator().forEachRemaining(myList::add);
+
+        long total = 0;
+        for (Country c : myList)
+        {
+            total = total + c.getPopulation();
+        }
+
+        System.out.println("The Total Population is " + total);
+        return new ResponseEntity<>("The Total Population is " + total, HttpStatus.OK);
+    }
+    // http://localhost:2019/population/min
+
+    @GetMapping(value = "/population/min", produces = {"application/json"})
+    public ResponseEntity<?> displayMinPop()
+    {
+        List<Country> myList = new ArrayList<>();
+
+        countryrepos.findAll().iterator().forEachRemaining(myList::add);
+
+        myList.sort((c1, c2) -> Long.compare(c1.getPopulation(), c2.getPopulation()));
+
+        Country minPop = myList.get(0);
+
+        return new ResponseEntity<>(minPop, HttpStatus.OK);
+    }
+    // http://localhost:2019/population/max
+    @GetMapping(value = "/population/max", produces = {"application/json"})
+    public ResponseEntity<?> displayMaxPop()
+    {
+        List<Country> myList = new ArrayList<>();
+
+        countryrepos.findAll().iterator().forEachRemaining(myList::add);
+
+        myList.sort((c1, c2) -> Long.compare(c2.getPopulation(), c1.getPopulation()));
+
+        Country maxPop = myList.get(0);
+
+        return new ResponseEntity<>(maxPop, HttpStatus.OK);
+    }
 }
